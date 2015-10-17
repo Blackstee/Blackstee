8bb475f28cf40ab67a6e816d53cad97ed22ab979
Blackstee <ya.nastuha24@yandex.ua>
2015-10-17 16:38:19 +0300

    for all

--- courses/prog_base/tasks/long2double/repr.c
+++ courses/prog_base/tasks/long2double/repr.c
@@ -1,33 +1,47 @@
 
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
+double long2double(long long);
+int main(){
+long long a = 0;
+double R;
+scanf("%lld", &a);
+R = long2double(a);
+printf("%.325llf", R);
+return 0;
+}
+double long2double(long long a)
+{
+    long long E = 0;    // Exponenta
+    short L, i;         //i - lichilnyk, L - znak
+    double M = 0;       // mantissa
+    double Mnorm = 0;   // mantissa u normalizovanomu
+    double R = 0;       //resultat
+    long long num = a;  //chislo
+    int n = -52;        //stepin
 
-int main (){
-    long long a; //number
-    short L, i;  //i - lichilnyk, L - znak
-    double M, R; // M - mantisa, R - result
-    int E;       // E - exponenta
-    a = 4620000000000000000;
-    if ((a>>63)==0){
+    //znahodjennya znaku
+    if (((num>>63)&1)== 0)
         L = 0;
-        E = (a>>52);
+        else
+        L =1;
+    //znahodjennya exponenti
+    E = num >> 52;
+    E = 0x00000000000007FF & E;
+
+    //znahodjennya mantissi
+    for (i = 0; i <= 51; i++) {
+        M+=(((num>>i)&1)*(pow((double)2,n)));
+        n++;
     }
-    else {
-        L=1;
-        E = abs((a>>52)+1);
-    }
-    for (i = 51; i >= 0; --i) {
-        M+=(((a>>i)&1)*(pow(2, i - 52)));
-    }
+    //znahodjennya mantissi u normalizovanomu viglyadi
+    Mnorm = M + 1.0;
+
     if ((E == 2047) &&  (M!=0.0)) return printf ("R = NaN");
     if ((E == 2047) && (M == 0.0) && (L == 1) )return printf ("R = -Infinity");
     if ((E == 2047) && (M == 0.0) && (L == 0))return printf ("R = Infinity");
-    if ((E > 0) && (E <2047)) {R = (pow(-1,L)* pow(2,E-1023)*(1+M));
-                printf("%.20llf", R);}
-    if ((E == 0) && (M !=0.0)) {R = (pow(-1, L)* pow(2, -1022)*(M));
-                    printf("%.325llf", R);}
+    //Normalizovane
+    if ((E > 0) && (E <2047)) {R = (pow((double)-1,L)* pow((double) 2, (double) (E-1023))*Mnorm); return (double) R;}
+    //Denormalizovane
+    if ((E == 0) && (M != 0)) {R = (pow((double)-1, L)* pow((double) 2, -1022)*M); return (double) R;}
     if ((E == 0) && (M == 0.0) && (L == 1))return printf("R = -0");
     if ((E == 0) && (M == 0.0) && (L == 0)) return printf ("R = 0");
-
 }
