ac14573802481cd6c0a5b22385a88fd92bd66f3e
Blackstee <ya.nastuha24@yandex.ua>
2015-11-08 21:42:51 +0200

    proc

--- courses/prog_base/tasks/string_simple/proc.c
+++ courses/prog_base/tasks/string_simple/proc.c
@@ -1,33 +1,40 @@
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
 
 char * process(char * resultStr, const char * textLines[],
                int linesNum, const char * extraStr) {
-    int i = 0, j;
-    char line[100];
-    int maxcount;
-    char *pline = line;
-    int word;
-    int count[linesNum];
-    for(j = 0; j < linesNum; j++){
-       * pline = * textLines[j];
-       while(line[i] == '0' && line[i] != '\0')      //Dohodimo do pershogo slova
-        i++;
-       word = 0;
-       while(line[i] != '\0') {
-         if(line[i] != '0' && line[i-1] =='0' && line[i-2] =='0'&& word == 0) {
-            word = 1;
-            count[j]++;
-         }else
-           if(line[i] == '0')
-              word = 0;
-       i++;
-  }
-  }
-  maxcount = count[0];
-  for(j = 0; j < linesNum; j++){
-      if (count[j] >= maxcount)
-        maxcount = count [j];
-  }
-  maxcount++;                                         //vrahuvannya pershogo slova
-    strcpy(resultStr, "This is result: ");
+    int i, j, a, s, sum = 0, l = 0;
+    char str[100];
+    str[0] = 0;
+    for(i = 0; i < linesNum; i++)
+    {
+       s = 0;
+       a = 0;
+       str[0] = 0;
+       for(j = 0; j < strlen(textLines[i]); j++)
+       {
+           if(*(textLines[i] + j) == '0')
+           {
+
+               if (a == 1)
+               {
+                  a = 0;
+                  s = s + 1;
+               } else
+                   a = 1;
+           }
+       }
+       s++;
+       if (s >= sum)
+       {
+           sum = s;
+           l = i;
+       }
+    }
+    sprintf(resultStr, "%i ", sum);
+    strcat(resultStr, textLines[l]);
+    return resultStr;
 }
